a6e6c4874be03a61f8d7e1fee8835298fc193a74
andre <andrlzpt@protonmail.com>
2025-05-03 22:58:07 -0300

    implementação do stage manager

--- src/game/constants/error.js
+++ src/game/constants/error.js
@@ -1,3 +1,8 @@
 export const FAILED_TO_FETCH_MANIFEST = (url) => `Failed to fetch manifest.json from ${url}`
-export const NOT_IMPLEMENTED = (someClass, someMethod) => `${someClass} must implement ${someMethod}`
-export const INVALID_ARGUMENT = (someClass, someArgument) => `${someClass}: Invalid or missing argument '${someArgument}'`
+export const NOT_IMPLEMENTED = (className, methodName) => `${className} must implement ${methodName}`
+export const INVALID_ARGUMENT = (className, argumentName) => `${className}: Invalid or missing argument '${argumentName}'`
+export const CONTAINER_ALREADY_EXISTS = (containerName) => `Container ${containerName} already exists`
+export const CONTAINER_DOES_NOT_EXIST = (containerName) => `Container ${containerName} does not exist`
+
+export const NOT_INITIALIZED = (className) => `${className} not initialized yet`
+export const IS_SINGLETON = (className) => `${className} is a singleton. Use ${className}.getInstance`
