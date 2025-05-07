a96f84eebf33c1357eef9de0c91d78829cdc33a7
andre <andrlzpt@protonmail.com>
2025-05-07 20:21:43 -0300

    implementando classe base e consertando tratamento de mensagem do erro invalid_argument para refletir o nome da classe do objeto

--- src/game/mapbuilding/MapTextureRenderer.js
+++ src/game/mapbuilding/MapTextureRenderer.js
@@ -1,10 +1,16 @@
-import { AbstractRenderer, Container } from 'pixi.js'
+import { AbstractRenderer, Container, Spritesheet } from 'pixi.js'
 import { INVALID_ARGUMENT, IS_ABSTRACT, NOT_IMPLEMENTED } from '../constants/error.js'
 
 export class MapTextureRenderer {
   constructor (renderer, spritesheet, tiles) {
     if (!(renderer instanceof AbstractRenderer)) {
-      throw new Error(INVALID_ARGUMENT(this.constructor.name, `renderer: ${typeof context}`))
+      throw new Error(INVALID_ARGUMENT(this.constructor.name, `renderer: ${renderer.constructor.name}`))
+    }
+    if (!(spritesheet instanceof Spritesheet)) {
+      throw new Error(INVALID_ARGUMENT(this.constructor.name, `renderer: ${spritesheet.constructor.name}`))
+    }
+    if (tiles?.length === 0) {
+      throw new Error(INVALID_ARGUMENT(this.constructor.name, `tiles length: ${tiles?.length}`))
     }
     if (new.target === MapTextureRenderer) {
       throw new Error(IS_ABSTRACT(this.constructor.name))
