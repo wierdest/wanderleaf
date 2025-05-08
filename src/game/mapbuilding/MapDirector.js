c644488391fd38f3fd8560fda03d33374230bb27
andre <andrlzpt@protonmail.com>
2025-05-08 20:01:56 -0300

    implementando isometric map director e adicionando método refine no map director para começar o trabalho assíncrono de construção do mapa

--- src/game/mapbuilding/MapDirector.js
+++ src/game/mapbuilding/MapDirector.js
@@ -3,21 +3,26 @@ import { INVALID_ARGUMENT, IS_ABSTRACT, NOT_IMPLEMENTED } from '../constants/err
 export class MapDirector {
   constructor (builder, progressCallback = () => {}) {
     if (new.target === MapDirector) {
-      throw new Error(IS_ABSTRACT(this.constructor.name));
+      throw new Error(IS_ABSTRACT(this.constructor.name))
     }
     if (!builder) {
       throw new Error(INVALID_ARGUMENT(this.constructor.name, `builder: ${builder.constructor.name}`))
     }
-    if (typeof this.progressCallback !== 'function') {
+    if (typeof progressCallback !== 'function') {
       throw new Error(INVALID_ARGUMENT(this.constructor.name, `progressCallback: ${typeof progressCallback}`))
     }
 
     this.builder = builder
     this.progressCallback = progressCallback
+    this.tiles = []
   }
 
   construct () {
     throw new Error(NOT_IMPLEMENTED(this.constructor.name, 'construct'))
+  }
+
+  async refine () {
+    throw new Error(NOT_IMPLEMENTED(this.constructor.name, 'refine'))
   }
 
   updateProgress (progress) {
