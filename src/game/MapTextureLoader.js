c1f77f5c2015de0a1c869ef4d105a5056b29f0b8
andre <andrlzpt@protonmail.com>
2025-05-10 21:14:15 -0300

    criação do map texture loader

---
+++ src/game/MapTextureLoader.js
@@ -0,0 +1,20 @@
+import { IsometricMapTextureRenderer } from './IsometricMapTextureRenderer.js'
+import { BaseLoader } from './loaders/BaseLoader.js'
+import { SpriteLoader } from './SpriteLoader.js'
+
+export class MapTextureLoader extends BaseLoader {
+  constructor (renderer, tiles) {
+    super()
+    this.tiles = tiles
+    this.renderer = renderer
+    this.spriteLoader = new SpriteLoader()
+  }
+
+  async _work ({ progressCallback }) {
+    const spritesheet = await this.spriteLoader.load({ entity: 'map' })
+    const isometricMapTextureRenderer = new IsometricMapTextureRenderer(this.renderer, spritesheet, this.tiles)
+    const texture = isometricMapTextureRenderer.render()
+    progressCallback(0.15)
+    return texture
+  }
+}
