1bb7105047ff7c3736b58f6b95fc3b82ecb38b14
andre <andrlzpt@protonmail.com>
2025-05-27 19:43:59 -0300

    safety checks no construtor do biome context

--- src/game/mapbuilding/BiomeContext.js
+++ src/game/mapbuilding/BiomeContext.js
@@ -1,5 +1,16 @@
+import { INVALID_ARGUMENT } from '../constants/errors.js'
+import { Bounds } from '../math/Bounds.js'
+
 export class BiomeContext {
   constructor (bounds, textureIds, ...args) {
+    if (!(bounds instanceof Bounds) || !(textureIds instanceof Array)) {
+      throw new Error(
+        INVALID_ARGUMENT(
+          this.constructor.name,
+                    `bounds: ${bounds?.constructor?.name}, size: ${textureIds?.constructor?.name}`
+        )
+      )
+    }
     this.bounds = bounds
     this.textureIds = textureIds
     this.args = args
